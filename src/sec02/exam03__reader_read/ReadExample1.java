2ea5e9c983a5de1270410996a1367ddf4fa39b80
Soyoung Ahn <clear94kr@hanmail.net>
2021-11-18 18:17:10 +0900

    211119 practice java!

---
+++ src/sec02/exam03__reader_read/ReadExample1.java
@@ -0,0 +1,33 @@
+package sec02.exam03__reader_read;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class ReadExample1 {
+
+	public static void main(String[] args) throws IOException {
+		Reader reader = null;
+		
+			reader = new FileReader("test.txt");
+			/*
+			int readData;			
+			while((readData = reader.read()) != -1) {
+				System.out.print((char)readData);			
+			}		*/
+		
+		
+			
+		
+		int readData1;
+		while(true) {
+			readData1 =  reader.read();
+			if(readData1 == -1) break;
+			System.out.print((char)readData1);
+		}
+		
+		reader.close();
+	
+	}
+}	
